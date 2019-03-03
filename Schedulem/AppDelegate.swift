d32854ca0f778dfa68a4a2e8476bf805522d7a91
Timothy Pearson <timo.555@icloud.com>
2019-03-03 16:25:20 +0100

    Initial Commit

---
+++ Schedulem/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Schedulem
+//
+//  Created by Timothy Pearson on 3/3/19.
+//  Copyright © 2019 Timothy Pearson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Schedulem/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Schedulem
+//
+//  Created by Timothy Pearson on 3/3/19.
+//  Copyright © 2019 Timothy Pearson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
