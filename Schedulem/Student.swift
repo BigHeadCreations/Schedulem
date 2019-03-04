c3525a9204906382dbac813dd987485dea5983ad
Timothy Pearson <timo.555@icloud.com>
2019-03-04 22:02:20 +0100

    cleanup

--- Schedulem/Student.swift
+++ Schedulem/Student.swift
@@ -8,7 +8,16 @@
 
 import Cocoa
 
-class Student: NSObject
+class Student
 {
+	var name : String? = nil
+	var male : Bool? = nil
+	var lastAssignment : Date? = nil
+	var nextAssignment : Date? = nil
+	var lastHH : Date? = nil
+	var nextHH : Date? = nil
+	
+	
+	
 
 }
