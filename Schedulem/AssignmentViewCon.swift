dd73ad50643bd1d51e5be59e94aca62fab69379b
Timothy Pearson <timo.555@icloud.com>
2019-03-04 22:55:09 +0100

    Rudimentary UI setup

---
+++ Schedulem/AssignmentViewCon.swift
@@ -0,0 +1,19 @@
+//
+//  AssignmentViewCon.swift
+//  Schedulem
+//
+//  Created by Timothy Pearson on 3/4/19.
+//  Copyright © 2019 Timothy Pearson. All rights reserved.
+//
+
+import Cocoa
+
+class AssignmentViewCon: NSViewController {
+
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		// Do view setup here.
+	}
+	
+}
---
+++ Schedulem/StudentViewCon.swift
@@ -0,0 +1,21 @@
+//
+//  StudentViewCon.swift
+//  Schedulem
+//
+//  Created by Timothy Pearson on 3/4/19.
+//  Copyright © 2019 Timothy Pearson. All rights reserved.
+//
+
+import Cocoa
+
+class StudentViewCon: NSViewController {
+
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		// Do view setup here.
+	}
+
+
+
+}
